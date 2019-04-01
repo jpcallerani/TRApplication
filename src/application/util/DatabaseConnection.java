fac16a5a9d878aef219b25b1d2a8da51b5a86935
Joao Silva <jpcallerani@gmail.com>
2019-04-01 11:09:00 -0300

    Adicionando método para pegar cod_sistema automático;

--- src/application/util/DatabaseConnection.java
+++ src/application/util/DatabaseConnection.java
@@ -4,24 +4,30 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
 import oracle.jdbc.OracleDriver;
+import oracle.jdbc.OraclePreparedStatement;
 
 public class DatabaseConnection {
 
-	private Connection con = null;
+	private Connection							con	= null;
+	private OraclePreparedStatement	_statement_st;
 
 	/**
 	 *
 	 * @return
+	 * @throws ClassNotFoundException
 	 * @throws SQLException
 	 */
 	public Connection Connect() {
 		try {
+
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+
 			Properties v_properties_prop;
 
 			v_properties_prop = new Properties();
@@ -29,8 +35,8 @@ public class DatabaseConnection {
 			v_properties_prop.put("password", Install.password);
 
 			con = DriverManager.getConnection(Install.url, v_properties_prop);
-		} catch (ClassNotFoundException e) {
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 		return con;
 	}
@@ -48,6 +54,7 @@ public class DatabaseConnection {
 			String url = getURL(tns);
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+
 			Properties v_properties_prop;
 
 			v_properties_prop = new Properties();
@@ -56,7 +63,7 @@ public class DatabaseConnection {
 
 			DriverManager.registerDriver(new OracleDriver());
 
-			con = DriverManager.getConnection(url, v_properties_prop);
+			this.con = DriverManager.getConnection(url, v_properties_prop);
 
 			Install.username = username;
 			Install.password = password;
@@ -65,7 +72,7 @@ public class DatabaseConnection {
 
 			this.createViewCmLogScripts();
 
-			con.close();
+			this.con.close();
 			retorno = 1;
 		} catch (Exception e) {
 		}
@@ -115,11 +122,11 @@ public class DatabaseConnection {
 	public void createViewCmLogScripts() {
 		try {
 			this.Connect();
-			Statement statement = con.createStatement();
+			Statement statement = this.con.createStatement();
 			String code = "create or replace view  V_CM_LOG_SCRIPTS as select rownum as id, usuario as usuario, versao as versao, tipo as tipo, objeto as objeto, erro as erro, data as data, rm as rm from cm_log_scripts";
 			statement.executeUpdate(code);
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 	}
 
@@ -129,11 +136,51 @@ public class DatabaseConnection {
 	public void dropViewCmLogScripts() {
 		try {
 			this.Connect();
-			Statement statement = con.createStatement();
+			Statement statement = this.con.createStatement();
 			String code = "drop view V_CM_LOG_SCRIPTS";
 			statement.executeUpdate(code);
 		} catch (Exception e) {
 
 		}
+	}
+
+	/**
+	 * 
+	 * @param Query
+	 *          a ser executada
+	 * @return Resultset do select executado
+	 * @throws SQLException
+	 */
+	public ResultSet Query(String query) throws SQLException {
+		ResultSet v_resultset_result;
+
+		this._statement_st = (OraclePreparedStatement) this.con.prepareStatement(query);
+		v_resultset_result = this._statement_st.executeQuery();
+		return v_resultset_result;
+	}
+  
+	/**
+	 * 
+	 * @param username
+	 * @return
+	 */
+	public boolean returnCodFromDatabase(String username) {
+		boolean resposta = true;
+		try {
+			ResultSet rs = this.Query("select cod_sistema from sfw_cm_schema where s_schema_owner = '" + username + "' and rownum = 1");
+			while (rs.next()) {
+				Install.cod_sistema = rs.getString("cod_sistema");
+			}
+			if (Install.cod_sistema == null || Install.cod_sistema.equalsIgnoreCase("")) {
+				if (username.toUpperCase().contains("RF")) {
+					Install.cod_sistema = "6";
+				} else {
+					resposta = false;
+				}
+			}
+		} catch (Exception e) {
+			resposta = false;
+		}
+		return resposta;
 	}
 }
