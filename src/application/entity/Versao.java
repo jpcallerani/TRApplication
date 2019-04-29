d0c8d59c4b0ed84dcc25fc1a46430d635fe0e5a0
Joao Silva <jpcallerani@gmail.com>
2019-04-28 21:49:16 -0300

    1) commitando a font

---
+++ src/application/entity/Versao.java
@@ -0,0 +1,32 @@
+package application.entity;
+
+import java.util.List;
+
+public class Versao {
+
+	private String id;
+	
+	private List<Objeto> objetos;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<Objeto> getObjetos() {
+		return objetos;
+	}
+
+	public void setObjetos(List<Objeto> objetos) {
+		this.objetos = objetos;
+	}
+	
+	
+
+	
+	 
+
+}
